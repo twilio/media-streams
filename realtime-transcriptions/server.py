a1a1fae8fe29415229725b9cde9932321828c284
Miguel Paris <mparis@twilio.com>
2019-03-17 21:14:50 +0100

    server.py: use SpeechClientBridge class and tidy up the code

--- realtime-transcriptions/server.py
+++ realtime-transcriptions/server.py
@@ -1,131 +1,49 @@
-import asyncio
-import os
-import websockets
-import sys
-import base64
-import queue
 
-from threading import Thread
 from flask import Flask, render_template
 from flask_sockets import Sockets
-from google.cloud import speech
+
+from SpeechClientBridge import SpeechClientBridge
 from google.cloud.speech import enums
 from google.cloud.speech import types
-from pyee import EventEmitter
-from pdb import set_trace as bp
 
-client = speech.SpeechClient()
 config = types.RecognitionConfig(
     encoding=enums.RecognitionConfig.AudioEncoding.MULAW,
     sample_rate_hertz=8000,
     language_code='en-US')
-streaming_config = types.StreamingRecognitionConfig(config=config,interim_results=True)
+streaming_config = types.StreamingRecognitionConfig(
+    config=config,
+    interim_results=False)
 
 app = Flask(__name__)
 sockets = Sockets(app)
-ee = EventEmitter()
-
 
 @app.route('/twiml', methods=['POST'])
 def returnTwiml():
     return render_template('streams.xml')
 
-
-class SheechToTextBridge:
-    def __init__(self):
-        self._queue = queue.Queue()
-        self._ended = False
-        self._thread = None
-
-    def terminate(self):
-        self._ended = True
-
-    def addRequest(self, buffer):
-        self._queue.put(types.StreamingRecognizeRequest(audio_content=bytes(buffer)))
-
-    def getRequests(self):
-        while not self._ended:
-            yield self._queue.get()
+def onResponse(response):
+    if not response.results:
         return
-    def printResponses(self, responses):
-        self._thread = Thread(target=self.listen_print_loop, args=[responses])
-        self._thread.start()
 
-    def listen_print_loop(self, responses):
-        """Iterates through server responses and prints them.
+    result = response.results[0]
+    if not result.alternatives:
+        return
 
-        The responses passed is a generator that will block until a response
-        is provided by the server.
-
-        Each response may contain multiple results, and each result may contain
-        multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-        print only the transcription for the top alternative of the top result.
-
-        In this case, responses are provided for interim results as well. If the
-        response is an interim one, print a line feed at the end of it, to allow
-        the next result to overwrite it, until the response is a final one. For the
-        final one, print a newline to preserve the finalized transcription.
-        """
-        num_chars_printed = 0
-        print("Before responses loop")
-        for response in responses:
-            #print("ANALYZING RESPONSE")
-            #bp()
-            if response.error:
-                print(response.error)
-
-            if not response.results:
-                continue
-
-            # The `results` list is consecutive. For streaming, we only care about
-            # the first result being considered, since once it's `is_final`, it
-            # moves on to considering the next utterance.
-            result = response.results[0]
-            if not result.alternatives:
-                continue
-
-            # Display the transcription of the top alternative.
-            transcript = result.alternatives[0].transcript
-
-            # Display interim results, but with a carriage return at the end of the
-            # line, so subsequent lines will overwrite them.
-            #
-            # If the previous result was longer than this one, we need to print
-            # some extra spaces to overwrite the previous result
-            overwrite_chars = ' ' * (num_chars_printed - len(transcript))
-
-            if not result.is_final:
-                sys.stdout.write(transcript + overwrite_chars + '\r')
-                sys.stdout.flush()
-
-                num_chars_printed = len(transcript)
-
-            else:
-                print(transcript + overwrite_chars)
-
-                # Exit recognition if any of the transcribed phrases could be
-                # one of our keywords.
-                #if re.search(r'\b(exit|quit)\b', transcript, re.I):
-                #    print('Exiting..')
-                #    break
-
-                num_chars_printed = 0
-        print("Ending responses loop")
+    transcription = result.alternatives[0].transcript
+    print("Transcription: " + transcription)
 
 @sockets.route('/')
-def pcmu(ws):
+def transcript(ws):
     print("WS connection openned")
-    bridge = SheechToTextBridge()
-    responses = client.streaming_recognize(streaming_config, bridge.getRequests())
-    bridge.printResponses(responses)
+    bridge = SpeechClientBridge(streaming_config, onResponse)
 
     while not ws.closed:
         message = ws.receive()
         if message is None:
             bridge.terminate()
-            return
-
+            break
         bridge.addRequest(message)
+
     print("WS connection closed")
 
 if __name__ == '__main__':
@@ -133,4 +51,5 @@ if __name__ == '__main__':
     from geventwebsocket.handler import WebSocketHandler
 
     server = pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler)
+    print("Server listening on: http://localhost:8080");
     server.serve_forever()
