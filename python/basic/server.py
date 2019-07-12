30d7dc8279fc0aead99021cd24e15e1a5ae309ea
Craig Dennis <cdennis@twilio.com>
2019-07-11 22:45:01 -0700

    Upgrade protocol python (#10)
    * Updates to 0.2.0 Call protocol
    
    * Updates to the 0.2.0 Call protocol

--- python/basic/server.py
+++ python/basic/server.py
@@ -4,10 +4,15 @@ from flask_sockets import Sockets
 import json
 import base64
 
+
 HTTP_SERVER_PORT = 8080
 
 app = Flask(__name__)
 sockets = Sockets(app)
+
+def log(msg, *args):
+    print(f"Media WS: ", msg, *args)
+
 
 @app.route('/twiml', methods=['POST'])
 def return_twiml():
@@ -16,21 +21,32 @@ def return_twiml():
 
 @sockets.route('/')
 def echo(ws):
-    print("Media WS: Connection accepted")
+    log("Connection accepted")
     count = 0
+    has_seen_media = False
     while not ws.closed:
         message = ws.receive()
         if message is None:
-            print('No message')
-            break
+            log("No message received...")
+            continue
 
         data = json.loads(message)
-        if data["sequenceNumber"] == "1":
-            print("Media WS: Received media and metadata: " + str(data))
-            print("Media WS: Additional messages from WebSocket are being suppressed.")
+        if data['event'] == "connected":
+            log("Connected Message received", message)
+        if data['event'] == "start":
+            log("Start Message received", message)
+        if data['event'] == "media":
+            if not has_seen_media:
+                log("Media message", message)
+                log("Additional media messages from WebSocket are being suppressed....")
+                has_seen_media = True
+        if data['event'] == "closed":
+            log("Closed Message received", message)
+            break
         count += 1
 
-    print("Media WS: Connection closed. Received a total of {} messages".format(count))
+    log("Connection closed. Received a total of {} messages".format(count))
+
 
 if __name__ == '__main__':
     from gevent import pywsgi
