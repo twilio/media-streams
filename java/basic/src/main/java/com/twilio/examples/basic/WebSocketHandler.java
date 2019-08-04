4d565cffe43e046272f0ca6101171c0ced275229
Craig Dennis <cdennis@twilio.com>
2019-08-04 12:39:38 -0700

    Restores basic to current state. See save-audio folder for this work

--- java/basic/src/main/java/com/twilio/examples/basic/WebSocketHandler.java
+++ java/basic/src/main/java/com/twilio/examples/basic/WebSocketHandler.java
@@ -11,26 +11,15 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 @WebSocket
 public class WebSocketHandler {
     final static Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
-    private File ulawFile;
-    FileOutputStream uLawFOS;
-
-    private String callSid;
-    private AudioFormat format = new AudioFormat(AudioFormat.Encoding.ULAW, 8000, 8, 1, 160, 50, true);
-
     final static Map<Session, Integer> messageCounts = new ConcurrentHashMap<>();
     final static Map<Session, Boolean> hasSessionSeenMedia = new ConcurrentHashMap<>();
 
@@ -40,14 +29,9 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    public void message(Session session, String message) {
+    public void message(Session session, String message) throws IOException {
         try {
             JSONObject jo = new JSONObject(message);
-            if (ulawFile == null) {
-                callSid = jo.getString("callSid");
-                ulawFile = new File(callSid + ".ulaw");
-                ulawFile.createNewFile();
-                uLawFOS = new FileOutputStream(ulawFile);
             String event = jo.getString("event");
             Boolean hasSeenMedia = hasSessionSeenMedia.getOrDefault(session, false);
             if (event.equals("connected")) {
@@ -64,42 +48,16 @@ public class WebSocketHandler {
                 }
 
             }
-            //OLD
-            String payload = jo.getString("payload");
-            byte[] decodedBytes = Base64.getDecoder().decode(payload);
-            uLawFOS.write(decodedBytes);
-            logger.info("Media WS: received media and metadata: {}", message);
-            logger.warn("Media WS: Additional messages from WebSocket are now being suppressed");
+            messageCounts.merge(session, 1, Integer::sum);
+
         } catch (JSONException e) {
             logger.error("Unrecognized JSON: {}", e);
-        } catch (IOException ioe) {
-            logger.error("IOException: {}", ioe);
         }
     }
 
-
     @OnWebSocketClose
     public void closed(Session session, int statusCode, String reason) {
-        try {
-            if (uLawFOS != null)
-                uLawFOS.close();
-            File wavFile = new File(callSid + ".wav");
-            FileOutputStream wavFOS = new FileOutputStream(wavFile);
-            if (!wavFile.exists()) {
-                wavFile.createNewFile();
-            }
-            AudioInputStream ais = new AudioInputStream(new FileInputStream(ulawFile), format, ulawFile.length() / 160);
-            AudioSystem.write(ais, AudioFileFormat.Type.WAVE, wavFOS);
-
-            if (wavFOS != null)
-                wavFOS.close();
-
-            wavFOS = null;
-            wavFile = null;
-            ulawFile = null;
-            uLawFOS = null;
-        } catch (Exception ex) {
-            logger.error("Exception in closing: {}", ex);
-        }
+        Integer count = messageCounts.remove(session);
+        logger.info("Media WS: Closed. Total of {} messages were sent.", count);
     }
 }
